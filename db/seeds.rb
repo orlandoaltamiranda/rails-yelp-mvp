b3f551e18318d82857d6705787ffbf79920a48d6
Orlando Altamiranda <orlandoaltamiranda@hotmail.com>
2021-08-15 19:41:20 -0400

    Last Changes - Add model, Controller and Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Eliminando toda la base de datos:"
+Restaurant.destroy_all
+Review.destroy_all
+
+10.times do
+  new_restaurant = Restaurant.new(name: Faker::Restaurant.name , category: categories.sample, phone_number: Faker::PhoneNumber.extension, address: Faker::Address.city)
+  new_restaurant.save                # Insert into DB and set id
+  yummy = Review.new(content: Faker::Quote.yoda, rating: rand(0..5))
+  yummy.restaurant = new_restaurant  # Set foreign key restaurant_id
+  yummy.save
+  new_restaurant.reviews             # Should contain the yummy review
+  yummy.restaurant
+end
